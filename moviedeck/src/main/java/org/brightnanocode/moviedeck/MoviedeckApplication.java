2033d99e1f92f913b96fb1c1809b7b41d144bd17
nandinigit33 <nandinids33@gmail.com>
2024-12-19 12:56:41 -0500

    first commit starter code

---
+++ moviedeck/src/main/java/org/brightnanocode/moviedeck/MoviedeckApplication.java
@@ -0,0 +1,13 @@
+package org.brightnanocode.moviedeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviedeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviedeckApplication.class, args);
+	}
+
+}
---
+++ moviedeck/src/test/java/org/brightnanocode/moviedeck/MoviedeckApplicationTests.java
@@ -0,0 +1,13 @@
+package org.brightnanocode.moviedeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviedeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
